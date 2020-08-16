f04c54483f65424e011c73e254c75fc5220aedb1
LucianMurmurache <48526455+lucianmurmurache@users.noreply.github.com>
2020-08-16 21:09:09 +0200

    Added computed methods

--- app/tetris/stageComputed.js
+++ app/tetris/stageComputed.js
@@ -1,1 +1,9 @@
-/* stageComp */
+export default {
+    computed: {
+        cellSize() { return this.stage.cellSize },
+        width() { return this.stage.width },
+        height() { return this.stage.height },
+        blocks() { return this.stage.blocks },
+        blockType() { return this.stage.blockType }
+    }
+}
